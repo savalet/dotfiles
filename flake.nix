32e64dda3753d830cc05c179c9f1b68fdd0e41da
Sigmanificient <edhyjox@gmail.com>
2024-10-21 12:10:07 +0200

    Replace nvin configuration + update flake lock

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     Neve.url = "github:savalet/Neve";
+
+    ecsls.url = "github:Sigmapitech/ecsls";
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -1,9 +1,41 @@
-{ inputs, pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
-  home.packages = with pkgs; [
-      inputs.Neve.packages.${system}.default
-      stylua
-      sumneko-lua-language-server
-      ripgrep
-  ];
+  home.file = {
+    nvim_conf = {
+      source = ./lua;
+      target = ".config/nvim/lua";
+      recursive = true;
+    };
+
+    clang_tidy = {
+      source = ./.clang-tidy;
+      target = ".clang-tidy";
+    };
+
+    clangd = {
+      source = ./.clangd;
+      target = ".clangd";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+
+    extraConfig = (builtins.readFile ./.vimrc);
+    plugins = [ pkgs.vimPlugins.lazy-nvim ];
+
+    extraPackages = with pkgs; let
+      ecsls-pkg = ecsls.packages.${pkgs.system}.default;
+    in
+    [
+      nil
+      lua-language-server
+      pyright
+      clang-tools
+      llvmPackages_latest.clang
+      nodejs
+      xclip
+      ecsls-pkg
+    ];
+  };
 }
