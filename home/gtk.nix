561a62471ff6f4c12086814060e0415b0eab9d4b
savalet <savinien.petitjean@gmail.com>
2025-10-06 14:15:46 +0200

    Add cursor theme

--- home/gtk.nix
+++ home/gtk.nix
@@ -9,6 +9,15 @@
     };
   };
 
+  gtk = {
+    enable = true;
+
+    cursorTheme = {
+      name = "Catppuccin-Macchiato-Dark";
+      package = pkgs.catppuccin-cursors.macchiatoDark;
+    };
+  };
+
   qt = {
     enable = true;
     platformTheme.name = "kvantum";
